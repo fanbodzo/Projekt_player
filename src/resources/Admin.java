79ea9203dba0095f55a5fe111d8f30c99acf9f6d
wiktoria <kurpytaw@gmail.com>
2024-11-21 15:22:32 +0100

    stworzenie klas dla kont uzytkownikow i adminow

---
+++ src/resources/Admin.java
@@ -0,0 +1,13 @@
+package resources;
+
+
+public class Admin extends Accounts {
+    private int adminId;
+
+    public Admin(String login, String password, int adminId) {
+        super(login, password);
+        this.adminId = adminId;
+    }
+
+
+}
